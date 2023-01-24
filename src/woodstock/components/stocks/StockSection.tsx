13ff6af80ecba500ae81328e30e68806ef75fcfa
Christophe <c.applanat@gmail.com>
2023-01-24 10:10:21 +0100

    stocks

--- src/woodstock/components/stocks/StockSection.tsx
+++ src/woodstock/components/stocks/StockSection.tsx
@@ -1,4 +1,4 @@
-import styles from "src/styles/Home.module.css"
+import styles from "./stocks.module.css"
 import { User, Wood } from "@prisma/client"
 import createUserStocks from "src/woodstock/mutations/createUserStock"
 
@@ -12,10 +12,12 @@ export default function StockSection({ stocks, user, admin }: Props) {
   const displayStocks = () => {
     return stocks.map((stock) => {
       return (
-        <div key={stock.id}>
-          <p>{stock.type}</p>
-          <p>{stock.price}</p>
-          <p>{stock.quantity}</p>
+        <div key={stock.id} className={styles.stockItems}>
+          <h4>{stock.type}</h4>
+          <div>
+            <p>Prix : {stock.price} €</p>
+            <p>Quantité : {stock.quantity} m3</p>
+          </div>
         </div>
       )
     })
@@ -24,7 +26,7 @@ export default function StockSection({ stocks, user, admin }: Props) {
   return (
     <section className={styles.managementSection}>
       <h2>Stocks</h2>
-      {displayStocks()}
+      <div className={styles.stockContainer}>{displayStocks()}</div>
       <button
         className={styles.button}
         onClick={async () => {
