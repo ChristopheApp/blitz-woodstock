5fb0f4cc5c02d061090743b4b59f757b4fb0f66e
Christophe <c.applanat@gmail.com>
2023-01-29 18:07:59 +0100

    get suppliers's wood quantity >0

--- src/woodstock/wood/queries/getSuppliersWoodByAdminId.ts
+++ src/woodstock/wood/queries/getSuppliersWoodByAdminId.ts
@@ -3,6 +3,7 @@ import db from "db"
 export default async function getSuppliersWoodByAdminId(adminId: string) {
   const woods = await db.wood.findMany({
     where: {
+      quantityPurchased: { gt: 0 },
       supplier: {
         userId: {
           has: adminId,
